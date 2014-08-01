42f0fa8c6160b37ad194d44043d38ec41cc4ec3f
Anil Mujagic <anil.mujagic@gmail.com>
2014-08-01 02:48:13 +0200

    Add parameter to build script to prepare packages for update

--- build/build.bat
+++ build/build.bat
@@ -21,8 +21,8 @@ md .\BuildOutput\Server
 xcopy ..\src\DeploymentCockpit.Server\bin\*.dll .\BuildOutput\Server\bin /I
 xcopy ..\src\DeploymentCockpit.Server\App_Start\*.cs .\BuildOutput\Server\App_Start /I
 copy ..\src\DeploymentCockpit.Server\Web.config .\BuildOutput\Server
-copy ..\src\DeploymentCockpit.Server\AppSettings.config .\BuildOutput\Server\AppSettings.config.sample
-copy ..\src\DeploymentCockpit.Server\ConnectionStrings.config .\BuildOutput\Server\ConnectionStrings.config.sample
+copy ..\src\DeploymentCockpit.Server\AppSettings.config .\BuildOutput\Server
+copy ..\src\DeploymentCockpit.Server\ConnectionStrings.config .\BuildOutput\Server
 copy ..\src\DeploymentCockpit.Server\index.html .\BuildOutput\Server
 xcopy ..\src\DeploymentCockpit.Server\app\* .\BuildOutput\Server\app /I /S /EXCLUDE:exclusions.txt
 xcopy ..\src\DeploymentCockpit.Server\assets\* .\BuildOutput\Server\assets /I /S /EXCLUDE:exclusions.txt
@@ -31,14 +31,22 @@ xcopy ..\src\DeploymentCockpit.Server\ext\* .\BuildOutput\Server\ext /I /S /EXCL
 md .\BuildOutput\JobRunner
 xcopy ..\src\DeploymentCockpit.JobRunner\bin\Release\DeploymentCockpit.JobRunner.exe* .\BuildOutput\JobRunner
 xcopy ..\src\DeploymentCockpit.JobRunner\bin\Release\*.dll .\BuildOutput\JobRunner
-copy ..\src\DeploymentCockpit.JobRunner\AppSettings.config .\BuildOutput\JobRunner\AppSettings.config.sample
-copy ..\src\DeploymentCockpit.JobRunner\ConnectionStrings.config .\BuildOutput\JobRunner\ConnectionStrings.config.sample
+copy ..\src\DeploymentCockpit.JobRunner\AppSettings.config .\BuildOutput\JobRunner
+copy ..\src\DeploymentCockpit.JobRunner\ConnectionStrings.config .\BuildOutput\JobRunner
 
 md .\BuildOutput\Target
 xcopy ..\src\DeploymentCockpit.Target\bin\Release\DeploymentCockpit.Target.exe* .\BuildOutput\Target
 xcopy ..\src\DeploymentCockpit.Target\bin\Release\*.dll .\BuildOutput\Target
-copy ..\src\DeploymentCockpit.Target\AppSettings.config .\BuildOutput\Target\AppSettings.config.sample
-copy ..\src\DeploymentCockpit.Target\ConnectionStrings.config .\BuildOutput\Target\ConnectionStrings.config.sample
+copy ..\src\DeploymentCockpit.Target\AppSettings.config .\BuildOutput\Target
+
+if "%1" == "update" (
+    rem Add .sample extension to avoid replacing files during update
+    ren .\BuildOutput\Server\AppSettings.config AppSettings.config.sample
+    ren .\BuildOutput\Server\ConnectionStrings.config ConnectionStrings.config.sample
+    ren .\BuildOutput\JobRunner\AppSettings.config AppSettings.config.sample
+    ren .\BuildOutput\JobRunner\ConnectionStrings.config ConnectionStrings.config.sample
+    ren .\BuildOutput\Target\AppSettings.config AppSettings.config.sample
+)
 
 .\tools\7z.exe a .\BuildOutput\DeploymentCockpit.zip .\BuildOutput\*
 rd .\BuildOutput\Database /S /Q
