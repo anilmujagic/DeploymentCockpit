66d15dece9d7faaa28b5db4ffbf98b9352a4c1ef
Anil Mujagic <anil.mujagic@gmail.com>
2014-10-03 16:54:08 +0200

    Set version to 0.1.0 and make DB snapshot

--- src/MasterAssemblyInfo.cs
+++ src/MasterAssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Reflection;
 [assembly: AssemblyCompany("Anil Mujagić")]
 [assembly: AssemblyCopyright("© Anil Mujagić")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyInformationalVersion("0.1.0")]
