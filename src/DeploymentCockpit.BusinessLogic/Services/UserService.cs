8f159288fe4b5aab58ab235dbbd37b138084437c
Anil Mujagic <anil.mujagic@gmail.com>
2014-10-15 23:07:14 +0200

    Add UserService

---
+++ src/DeploymentCockpit.BusinessLogic/Services/UserService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DeploymentCockpit.Interfaces;
+using DeploymentCockpit.Models;
+
+namespace DeploymentCockpit.Services
+{
+    public class UserService : CrudService<User>, IUserService
+    {
+        public UserService(IUnitOfWorkFactory unitOfWorkFactory)
+            : base(unitOfWorkFactory)
+        {
+        }
+
+        public User GetByUsername(string username)
+        {
+            using (var uow = _unitOfWorkFactory.Create())
+            {
+                return uow.Repository<User>()
+                    .GetAll(u => u.Username == username)
+                    .SingleOrDefault();
+            }
+        }
+    }
+}
---
+++ src/DeploymentCockpit.Core/Interfaces/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DeploymentCockpit.Models;
+
+namespace DeploymentCockpit.Interfaces
+{
+    public interface IUserService : ICrudService<User>
+    {
+        User GetByUsername(string username);
+    }
+}
