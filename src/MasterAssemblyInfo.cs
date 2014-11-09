6b7904ac4309af57e53373de0fb585b148d3b6d8
Anil Mujagic <anil.mujagic@gmail.com>
2014-11-09 18:59:12 +0100

    Version 0.7.0

--- src/MasterAssemblyInfo.cs
+++ src/MasterAssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Reflection;
 [assembly: AssemblyCompany("Anil Mujagić")]
 [assembly: AssemblyCopyright("© Anil Mujagić")]
 
-[assembly: AssemblyVersion("0.6.0.0")]
-[assembly: AssemblyFileVersion("0.6.0.0")]
-[assembly: AssemblyInformationalVersion("0.6.0")]
+[assembly: AssemblyVersion("0.7.0.0")]
+[assembly: AssemblyFileVersion("0.7.0.0")]
+[assembly: AssemblyInformationalVersion("0.7.0")]
