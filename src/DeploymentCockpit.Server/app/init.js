082cd680bae079bfc0501ea69710c98fe15fb344
Anil Mujagic <anil.mujagic@gmail.com>
2015-03-17 15:06:36 +0100

    Hide toast close button

--- src/DeploymentCockpit.Server/app/init.js
+++ src/DeploymentCockpit.Server/app/init.js
@@ -1,6 +1,4 @@
 ﻿$(function () {
-    //toastr.options.positionClass = "toast-bottom-right";
-    toastr.options.closeButton = true;
     toastr.options.showDuration = 200;
     toastr.options.hideDuration = 200;
 });
